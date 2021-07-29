38da0c852662dbf88c2bfb6b24b1f3ba5786ed6f
vivostanoev <vivostanoev@gmail.com>
2021-07-30 01:10:09 +0300

    add test-data directory

--- test/pageobjects/todo-page.js
+++ test/pageobjects/todo-page.js
@@ -1,3 +1,8 @@
 module.exports = {
-    
+    addItemToList: (item) => {
+
+    },
+    deleteItemFromList: (item) => {
+        
+    }
 }
---
+++ test/test-data/item.js
@@ -0,0 +1,12 @@
+module.exports = {
+    items:{
+        item: {
+            shortDescription: "Cook dinner",
+            status: "Active"
+        },
+        item: {
+            shortDescription: "Call Anna",
+            status: "Complited"
+        }
+    }
+}
